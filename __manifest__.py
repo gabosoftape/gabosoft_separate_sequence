3308cf599da0ac41905a32fac3e53a0bfed97fc9
gabriel pabon <gabosoft.ape@gmail.com>
2018-12-04 14:27:50 -0500

    test v1

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+{
+    'name' : 'SW - RFQ Separate Sequence',
+    'version' : '11.0.1.0',
+    'category' : 'Purchase',
+    'author' : 'Smart Way Business Solutions',
+    'website' : 'https://www.smartway-jo.com',
+    'license':  "Other proprietary",
+    'summary': """Add a special sequence to your RFQs""",
+    'data': ['sequence.xml'],
+    'depends' : ['base', 'purchase'],
+    'images':  ["static/description/image.png"],
+    'installable': True,
+    'auto_install': False,
+}
